2e08a392c8f6f73dd25caab982b64f80006d1700
Carlos Kenny <carlos.kenny.ca@gmail.com>
2020-09-06 01:32:24 -0300

    validação do type da transação

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,10 +15,15 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type}: Request ): Transaction {
+    if(!["income","outcome"].includes(type)){
+      throw new Error("Tipo de transação inválida, tente novamente!");
+
+    }
+
     const saldo = this.transactionsRepository.getBalance().total;
 
     if(type === "outcome" && saldo < value) {
-      throw new Error("Você não possui saldo suficiente!")
+      throw new Error("Você não possui saldo suficiente!");
     }
 
     const transaction = this.transactionsRepository.create({title, value, type});
